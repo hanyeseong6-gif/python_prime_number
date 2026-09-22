b7c4fb89c31bfe357b7e761292d12982c5a448f6
h1222 <hanyeseong6@gmail.com>
2026-09-22 17:34:21 +0900

    feat: 2부터100까지소수판별기본로직 구현

--- prime_number.py
+++ prime_number.py
@@ -2,3 +2,13 @@
 print("1부터 100 사이의 소수를 구합니다.")
 for num in range(1, 101):
     pass # 기본 반복문 틀 작성
+# prime-number.py - 소수 구하기 기본로직완성
+print("1부터 100 사이의소수를구합니다.")
+for num in range(2, 101):
+    is_prime = True
+    for i in range(2, num):
+        if num % i == 0:
+            is_prime = False
+            break
+    if is_prime:
+        print(num, end=" ")
